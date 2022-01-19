726da6514dbf56eefe58e213225237cdd65a6c7a
lukew3 <lukew25073@gmail.com>
2022-01-19 17:13:35 -0500

    enabled linking to p5 projects

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const pug = require('pug');
-const fs = require('fs');
+const fs = require('fs-extra');
 const chokidar = require('chokidar');
 const express = require('express');
 const open = require('open');
@@ -25,26 +25,39 @@ const makeDist = () => {
 		if (err) console.log(err);
 	});
 	// copy user css and js files
-	fs.copyFile('./src/styles.css', './dist/styles.css', (err) => {
+	fs.copyFile('./src/style.css', './dist/style.css', (err) => {
 		if (err) console.log(err);
 	});
+	/*
+	 * Removed since no js outside of p5 will be needed
 	fs.copyFile('./src/main.js', './dist/main.js', (err) => {
 		if (err) console.log(err);
 	});
+	*/
 }
 
 const build = async () => {
 	console.log('building new version...');
 	makeDist();
 	let config = JSON.parse(fs.readFileSync('./src/config.json'));
-	fs.readdir('./src/projects', (err, contents) => {
+	fs.readdir('./src/projects', (err, projectDirs) => {
 		// contents should all be directories with the name of the project
 		// Add all directory names to config.projects
-		config.projects = contents;
+		config.projects = projectDirs;
 		console.log(config);
 		let fn = pug.renderFile('./src/index.pug', config);
 		fs.writeFile('./dist/index.html', fn, (err)=>{
 			if (err) console.log(err);
+		});
+		projectDirs.forEach((projectName) => {
+			let fn = pug.renderFile('./src/sketch.pug', {projectName});
+			fs.writeFile(`./dist/${projectName}.html`, fn, (err) => {
+				if (err) console.log(err);
+			});
+			// copy project folder to dist
+			fs.copy(`./src/projects/${projectName}`, `./dist/${projectName}`, (err) => {
+				if (err) console.log(err);
+			});
 		});
 	});
 }
